e4bbb274bff05313408ead098195d07dff38664e
mandarin41 <tyugai96@gmail.com>
2018-02-27 22:28:59 +0700

    keksf

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
 from telegram.ext import Updater
 updater = Updater(token='528744932:AAEPt-yfHBZbNQ9aMIlAUyuMSTz-QilXM6M')
+url = http://api.rasp.nsuem.ru/?controller=Times&action=GetTime&key=80a07cadffa1169625e3d4849bd31e793e05e81a
 
 dispatcher = updater.dispatcher
 
 import logging
+import requests
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(bot, update):
      bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 def r4751(bot, update):
-	 t = http://api.rasp.nsuem.ru/?controller=Times&action=GetTime&key=80a07cadffa1169625e3d4849bd31e793e05e81a 
-	 bot.send_message(chat_id=update.message.chat_id, text=t)
+	 response = requests.get(url)
+	 bot.send_message(chat_id=update.message.chat_id, text=response.json())
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
